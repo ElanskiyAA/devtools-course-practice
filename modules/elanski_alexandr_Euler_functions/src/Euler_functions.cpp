e197e0811120a9c314118a17425451589b62234d
ElanskiyAA <sasaelan1@gmail.com>
2022-04-25 20:05:36 +0300

    fix 0.5

--- modules/elanski_alexandr_Euler_functions/src/Euler_functions.cpp
+++ modules/elanski_alexandr_Euler_functions/src/Euler_functions.cpp
@@ -6,13 +6,16 @@
 
 int Euler::Function_Euler(int number) {
     int result = number;
-    for (int i = 2; i * i <= number; ++i)
+    for (int i = 2; i * i <= number; ++i) {
         if (number % i == 0) {
-            while (number % i == 0)
+            while (number % i == 0) {
                 number /= i;
+            }
             result -= result / i;
         }
-    if (number > 1)
+    }
+    if (number > 1) {
         result -= result / number;
+    }
     return result;
 }
